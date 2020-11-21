f5703b207fb8ac22eedf6906fef7a8fdb9b2cdb1
EvgeniyMay <yevgeniy.may@gmail.com>
2020-11-21 13:12:11 +0200

    Annotation of ShipsApiController changed to RestController

--- src/main/java/com/learning/ListOfShips/controller/ShipsApiController.java
+++ src/main/java/com/learning/ListOfShips/controller/ShipsApiController.java
@@ -3,16 +3,15 @@ package com.learning.ListOfShips.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.learning.ListOfShips.model.Ship;
 import com.learning.ListOfShips.repository.ShipRepository;
 
-@Controller
+@RestController
 @RequestMapping(path = "API")
 public class ShipsApiController {
 
@@ -25,13 +24,13 @@ public class ShipsApiController {
 	
 	
 	@GetMapping("ships")
-	public @ResponseBody Iterable<Ship> getShips() {
+	public Iterable<Ship> getShips() {
 
 		return shipRepository.findAll();
 	}
 	
 	@GetMapping("ships/{id}")
-	public @ResponseBody Optional<Ship> getShip(@PathVariable int id){
+	public Optional<Ship> getShip(@PathVariable int id){
 		return shipRepository.findById(id);
 	}
 }
